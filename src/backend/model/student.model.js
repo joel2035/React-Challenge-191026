23a95d022e95468230003b841946dc5f30e6f76f
pakdev1993 <>
2019-12-03 20:52:41 +0100

    [add] building Student mongo model

---
+++ src/backend/model/student.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const studentShema = new Schema({
+    nom:  {}= null, 
+        prenom = null, 
+        promo = null, 
+        descCusrus = null, 
+        email = null, 
+        competenceNote = null
+    
+})
--- src/backend/server.js
+++ src/backend/server.js
@@ -2,10 +2,24 @@ const express = requir('express')
 const app = express()
 const bodyParser  = require('body-parser')
 const cors = require('cors')
-const PORT = 4000
+const mongoose = require('mongoose')
 
+const PORT = 4000
+const DB_LOCAL_IP = '127.0.0.1:27017'
+const DB_NAME = 'React_Challenge'
+
+//brancher cors
 app.use(cors())
+//brancher le parseur d'HttpRequest
 app.use(bodyParser.json())
+
+//connection a la base mongo
+mongoose.connect('mongodb://' + DB_LOCAL_IP + '/' + DB_NAME)
+
+//ouvrir & deleguer la gestion de la connection a nodemon
+mongoose.once('open', function() {
+    console.log('MongoDB database connection established')
+})
 
 app.listen(PORT, function() {
     console.log("server runinf port: " + PORT)
