b7be92795ec353bc42a9693bd319b36f15c214d6
pakdev1993 <pakrstic.git@gmail.com>
2020-03-11 16:08:35 +0100

    [debug] server

--- backend/server.js
+++ backend/server.js
@@ -48,5 +48,5 @@ app.use('/note', noteRouter)
 //lancer le serv
 const serv_port = "27017" //process.env.SERV_PORT
 app.listen(serv_port, function() {
-    console.log("server runing PORT: " + serv_port)
+    console.log("server accessible sur le PORT: " + serv_port)
 })
